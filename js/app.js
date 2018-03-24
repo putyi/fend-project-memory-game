1df827ba7fe4fa093cf03a6b258350cb4a147e7d
Csiszer Istvan <fadingcircles@gmail.com>
2018-03-24 17:10:32 +0200

    Loading the DOM with shuffled cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const arrayOfCards = [
+    "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt",
+    "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb",
+    "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"
+];
 
 /*
  * Display the cards on the page
@@ -11,22 +15,40 @@
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+function shuffle(arrayOfCards) {
+    var currentIndex = arrayOfCards.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        temporaryValue = arrayOfCards[currentIndex];
+        arrayOfCards[currentIndex] = arrayOfCards[randomIndex];
+        arrayOfCards[randomIndex] = temporaryValue;
     }
 
-    return array;
+    return arrayOfCards;
+    //in case you want to check the shuffeled arrayOfCards
+    //console.log(arrayOfCards);
 }
 
+function addToDom() {
+    shuffle(arrayOfCards);
+    const deck = document.querySelector(".deck");
 
-/*
+    for (let i = 0; i <= arrayOfCards.length - 1; i++) {
+        let card = document.createElement("li");
+        card.className = ("card");
+        card.id = i;
+        deck.appendChild(card);
+        let icon = document.createElement("i");
+        icon.className = arrayOfCards[i];
+        card.appendChild(icon);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', addToDom);
+
+ /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
